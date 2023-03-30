2d89c3e70e6a86d7b1c925b57a0dc664cdff5ffb
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-03-30 16:58:53 -0500

    02 - functional components

--- 01-setup/src/App.js
+++ 01-setup/src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Heading() {
+  let title = 'Hello World';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    // h1 with style
+    <h1 style={{ fontSize: '16px' }}>{title}</h1>
+  )
+};
 
-export default App;
+export default Heading;
