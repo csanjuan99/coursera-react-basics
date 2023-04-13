1ecac3cdf63f6c160eeb5078f2c4ff368ffaedb7
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-13 01:05:50 -0500

    06.1 - styling_jsx

--- 05-props_children/src/components/Bag.js
+++ 05-props_children/src/components/Bag.js
@@ -1,9 +1,20 @@
 // it's works like slot in Vue.js
 function Bag(props) {
+    const bag = {
+        width: "200px",
+        height: "200px",
+        border: "1px solid black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "0 auto",
+        backgroundColor: "yellow"
+    }
     return (
-        <>
+        <div style={bag}>
             {props.children}
-        </>
+        </div>
     )
 }
 
