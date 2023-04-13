3916cf80d65b39eb2b36e763f243f0743a5201f5
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-13 01:08:21 -0500

    06.2 - styling_using_className

--- 05-props_children/src/components/Bag.js
+++ 05-props_children/src/components/Bag.js
@@ -1,18 +1,20 @@
+import bag from "../css/bag.css"
+
 // it's works like slot in Vue.js
 function Bag(props) {
-    const bag = {
-        width: "200px",
-        height: "200px",
-        border: "1px solid black",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        margin: "0 auto",
-        backgroundColor: "yellow"
-    }
+    // const bag = {
+    //     width: "200px",
+    //     height: "200px",
+    //     border: "1px solid black",
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     flexDirection: "column",
+    //     margin: "0 auto",
+    //     backgroundColor: "yellow"
+    // }
     return (
-        <div style={bag}>
+        <div className="bag">
             {props.children}
         </div>
     )
