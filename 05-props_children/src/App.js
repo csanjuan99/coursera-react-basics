7f56254992cb66655fffcd34dd491913168588a5
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-13 01:19:57 -0500

    06.3 - extending_children_functionality

--- 05-props_children/src/App.js
+++ 05-props_children/src/App.js
@@ -5,6 +5,9 @@ import Pear from "./components/Pear";
 function App() {
   return (
     <div className="App">
+      <span>One component one line</span>
+      <Bag children={<Apple number={5} />} />
+      <span>Many components open end tag</span>
       <Bag>
         <Apple number={5} />
         <Pear name="John" />
