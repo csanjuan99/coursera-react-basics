1faa1cffeb9923edca3e8dffade8f5dd0cb446aa
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-13 14:57:45 -0500

    06.5 - embed_jsx_to_element

--- 05-props_children/src/components/Copyright.js
+++ 05-props_children/src/components/Copyright.js
@@ -1,7 +1,12 @@
+import logo from '../assets/logo.png'
+
+const Logo = () => <img src={logo} alt="logo" />;
+
 const Copyright = () => {
     return (
         <div>
             <p>© 2021</p>
+            <Logo />
         </div>
     );
 }
