7066692c65fbcaca8f0e460bef4cf526cbd17414
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2023-04-13 00:46:51 -0500

    05 - passing_props

---
+++ 04-principles_component/src/App.js
@@ -0,0 +1,11 @@
+import Heading from "./components/Heading";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading title="Hello World" />
+    </div>
+  );
+}
+
+export default App;
